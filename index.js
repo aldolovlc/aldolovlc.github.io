e582613c53af481efcb0caff7fabeb77d9f9d1ae
Alberto <aldolo.vlc@gmail.com>
2022-03-21 21:47:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,21 +6,29 @@ let bounds;
 
 const ordinales = ['Primera', 'Segunda', 'Tercera', 'Cuarta', 'Quinta', 'Sexta', 'Séptima'];
 
+const ayuda = ['Hola David. En este detector de bolas de dragón podrás ver dónde aparecerán el resto de bolas.',
+               'Las bolas que aparecen dando saltos están preparadas para ir en su búsqueda.',
+               'Las bolas que aparecen fijas, ya están recuperadas. Seguirán en el mismo lugar donde las conseguiste.',
+               'Si pulsas en cada bola, aparecerá información y pistas para dar con ella.',
+               '¡Mucha suerte!'];
+               
+let ayudaPos = 0;
+
 const bolas = [
               { pos: {lat:40.47593894308782, lng:-0.40152796950835784}, show : true, animate : false,
-                texto : '<p>Primera.</p>' },
+                texto : 'Primera.' },
               { pos: {lat:39.47593894308782, lng:-10.40152796950835784}, show : false, animate : true,
-                texto : '<p>Segunda.</p>' },
+                texto : 'Segunda.' },
               { pos: {lat:38.47593894308782, lng:-11.40152796950835784}, show : true, animate : false,
-                texto : '<p>Tercera.</p>' },
+                texto : 'Tercera.' },
               { pos: {lat:37.47593894308782, lng:-16.40152796950835784}, show : true, animate : true,
-                texto : '<p>Cuarta.</p>' },
+                texto : 'Cuarta.' },
               { pos: {lat:36.47593894308782, lng:-20.40152796950835784}, show : true, animate : false,
-                texto : '<p>Para obtener esta bola, hay que ganar tres partidas de piedra, papel o tijera contra Paco.</p>' },
+                texto : 'Para obtener esta bola, hay que ganar tres partidas de piedra, papel o tijera contra Paco.' },
               { pos: {lat:33.47593894308782, lng:-40.40152796950835784}, show : true, animate : true,
-                texto : '<p>Sexta.</p>' },
+                texto : 'Sexta.' },
               { pos: {lat:30.47593894308782, lng:-3.40152796950835784}, show : true, animate : false,
-                texto : '<p>Septima.</p>' }
+                texto : 'Septima.' }
               ];
 
 function centrarBolaGUI(controlDiv) {
@@ -53,6 +61,98 @@ function centrarBolaGUI(controlDiv) {
         }
     });
     controlDiv.appendChild(etiqueta);
+}
+
+function bulmaDiceGUI(controlDiv){
+    const divTextoYBotones = document.createElement('div');
+    const texto = document.createElement('p');
+    const divBotones = document.createElement('div');
+    const divMostrar = document.createElement('div');
+    const img = document.createElement('img');
+    const ba = document.createElement('button');
+    const bz = document.createElement('button');
+    const bm = document.createElement('button');
+    const bo = document.createElement('button');
+    
+    divTextoYBotones.appendChild(img);
+    divTextoYBotones.appendChild(texto);
+    divTextoYBotones.appendChild(divBotones);
+    divBotones.appendChild(ba);
+    divBotones.appendChild(bo);
+    divBotones.appendChild(bz);
+    controlDiv.appendChild(divTextoYBotones);
+    divMostrar.appendChild(bm);
+    controlDiv.appendChild(divMostrar);
+    
+    divTextoYBotones.setAttribute('id', 'bul1');
+    divMostrar.setAttribute('id', 'bul2');
+    texto.setAttribute('id', 'textoAyuda');
+    
+    img.setAttribute('src', 'img/bulma.png');
+    img.setAttribute('alt', 'Foto de Bula');
+    
+    ba.setAttribute('id', 'bota');
+    ba.setAttribute('value', '-1');
+    ba.style.visibility = 'hidden';
+    ba.addEventListener('click', botonHandler);
+    ba.appendChild(document.createTextNode('⬅'));
+    
+    bz.setAttribute('id', 'botz');
+    bz.setAttribute('value', '1');
+    bz.addEventListener('click', botonHandler);
+    bz.appendChild(document.createTextNode('➡'));
+    
+    bo.setAttribute('value', '0');
+    bo.addEventListener('click', botonHandler);
+    bo.appendChild(document.createTextNode('Ocultar'));
+    
+    bm.setAttribute('value', '2');
+    bm.addEventListener('click', botonHandler);
+    bm.appendChild(document.createTextNode('Mostrar'));
+    
+    texto.appendChild(document.createTextNode(ayuda[0]));
+
+    switch(localStorage.getItem('ayuda')){
+        case '0':
+            divTextoYBotones.style.display = 'none';
+            break;
+        case '1':
+        default:
+            divMostrar.style.display = 'none';
+    }
+}
+    
+function botonHandler(a){
+    const ta = document.getElementById('textoAyuda');
+    switch(this.value){
+        case "-1":
+            if(ayudaPos > 0){
+                ayudaPos--;
+                ta.textContent = ayuda[ayudaPos];
+            }
+            if(ayudaPos == 0) document.getElementById('bota').style.visibility = 'hidden';
+            if(ayudaPos == ayuda.length-2) document.getElementById('botz').style.visibility = 'visible';
+            break;
+        case "0":
+            document.getElementById('bul1').style.display = 'none';
+            document.getElementById('bul2').style.display = 'block';
+            localStorage.setItem('ayuda','0');
+            break;
+        case "1":
+            if(ayudaPos < ayuda.length-1){
+                ayudaPos++;
+                ta.textContent = ayuda[ayudaPos];
+            }
+            if(ayudaPos == ayuda.length-1) document.getElementById('botz').style.visibility = 'hidden';
+            if(ayudaPos == 1) document.getElementById('bota').style.visibility = 'visible';
+            break;
+        case "2":
+            document.getElementById('bul1').style.display = 'block';
+            document.getElementById('bul2').style.display = 'none';
+            localStorage.setItem('ayuda','1');
+            break;
+        default:
+    }
 }
 
 function iniciarBola(){
@@ -134,8 +234,12 @@ function initMap() {
         }
     }
     if(bola == null) mapa.fitBounds(bounds, 30);
-    const divCentrarBola = document.createElement("div");
+    const divCentrarBola = document.createElement('div');
     divCentrarBola.setAttribute('id', 'divCentrarBola');
     centrarBolaGUI(divCentrarBola);
+    const divBulmaDice = document.createElement('div');
+    divBulmaDice.setAttribute('id', 'divBulmaDice');
+    bulmaDiceGUI(divBulmaDice);
     mapa.controls[google.maps.ControlPosition.TOP_CENTER].push(divCentrarBola);
+    mapa.controls[google.maps.ControlPosition.BOTTOM_LEFT].push(divBulmaDice);
 }
